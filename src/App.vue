cebe42b0ed09afed4f2b4775f59e10a76a545a3f
jianhui <78072865+jiajianhui@users.noreply.github.com>
2025-05-18 15:00:29 +0800

    核心功能开发完成

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,40 @@
 <script setup>
+import { ref } from 'vue';
+
+const newTask = ref('')
+
+
+// 添加函数
+function Add() {
+
+  // 判断
+  if (newTask.value.trim() === "") {
+    return alert("Please enter the task")
+  }
+
+  // 添加任务
+  taskList.value.push({
+    id: taskList.value.length + 1, 
+    name: newTask.value, 
+    completed: false
+  })
+
+
+  // 清空输入框
+  newTask.value = ""
+}
+
+// 删除函数
+function Del(index) {
+  taskList.value.splice(index, 1)
+}
+
+// 任务列表
+const taskList = ref([
+  { id: 1, name: 'Task 1', completed: false },
+  { id: 2, name: 'Task 2', completed: false },
+  { id: 3, name: 'Task 3', completed: false },
+])
 
 </script>
 
@@ -10,18 +46,20 @@
 
     <!-- 输入框 -->
     <div class="inputBox">
-      <input type="text" placeholder="Add a new task" />
-      <button>Add</button>
+      <input v-model="newTask" type="text" placeholder="Add a new task" />
+      <button @click="Add">Add</button>
 
     </div>
 
     <!-- 任务列表 -->
-    <div class="taskBox">
-      <div class="taskItem" v-for="_ in 10">
-        <input type="checkbox" />
-        <span>Task 1</span>
-        <button>Delete</button>
+    <div class="taskBox normal" v-for="(item, index) in taskList" key="item.id">
+
+      <div class="left" :class=" item.completed && 'completed' ">
+        <input v-model="item.completed" type="checkbox" />
+        <span>{{ item.name }}</span>
       </div>
+
+      <button @click="Del(index)">Delete</button>
     </div>
 
   </div>
@@ -39,5 +77,22 @@
 
 .inputBox {
   padding: 60px 0;
+}
+
+
+.taskBox {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 400px;
+}
+
+.normal {
+  font-size: 20px;
+  padding: 10px 0;
+}
+.completed > span {
+  text-decoration: line-through;
+  opacity: 0.4;
 }
 </style>
