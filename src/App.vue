a3eb7d49b3bff64e4f10088ff83b196971999157
jianhui <78072865+jiajianhui@users.noreply.github.com>
2025-05-18 14:16:32 +0800

    基础布局搭建

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main">
+
+    <!-- 标题 -->
+    <h1>ToDo App</h1>
+
+    <!-- 输入框 -->
+    <div class="inputBox">
+      <input type="text" placeholder="Add a new task" />
+      <button>Add</button>
+
+    </div>
+
+    <!-- 任务列表 -->
+    <div class="taskBox">
+      <div class="taskItem" v-for="_ in 10">
+        <input type="checkbox" />
+        <span>Task 1</span>
+        <button>Delete</button>
+      </div>
+    </div>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  height: 100vh;
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.inputBox {
+  padding: 60px 0;
 }
 </style>
